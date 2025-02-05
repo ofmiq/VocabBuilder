59e267cc12fb3da0f8eb946b6140bbbdcf74ec26
ofmi <goncarm354@gmail.com>
2025-02-05 23:18:44 +0200

    Reorganized project files into structured directories

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,11 @@
 /**
  * @brief main entry point for the VocabBuilder application.
  */
-int main(int argc, char *argv[])
-{
+
+int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     MainWindow window;
-    window.setWindowTitle(QObject::tr("VocabBuilder"));
     window.show();
     return app.exec();
 }
+
